ecf7bd68b1242f44ca4aecf3c4ec04151f263476
Juan Camilo Florez Paez <jcflorezpaez@gmail.com>
2025-04-17 19:40:28 -0500

    reafactor

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -3,7 +3,7 @@ import { OrderStatus } from '../utils/enums';
 import OrderController from '../controllers/order.controller';
 
 const router = Router();
-// Obtener todas las órdenes
+
 router.get('/', OrderController.getOrders);
 router.get('/:id', OrderController.getOrderById);
 router.get('/user/:userId', OrderController.getOrdersByUserId);
