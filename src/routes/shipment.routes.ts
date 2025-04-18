88a99a6ae88e300ee4c05ce71b291db44aaa3d7d
Juan Camilo Florez Paez <jcflorezpaez@gmail.com>
2025-04-18 15:11:18 -0500

    feat: se añade bloqueo por rol

--- src/routes/shipment.routes.ts
+++ src/routes/shipment.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import shipmentController from '../controllers/shipment.controller';
-import { validateToken } from '../middlewares/auth.middleware';
+import { UserRole } from '../utils/enums';
+import { validateToken, hasRole } from '../middlewares/auth.middleware';
 
 const router = Router();
 
-router.get('/', validateToken, shipmentController.getShipments);
-router.get('/:id', validateToken, shipmentController.getShipmentById);
-router.get('/order/:orderId', validateToken, shipmentController.getShipmentsByOrderId);
-router.post('/', validateToken, shipmentController.createShipment);
-router.put('/:id', validateToken, shipmentController.updateShipment);
-router.delete('/:id', validateToken, shipmentController.deleteShipment);
+router.get('/', validateToken, hasRole(UserRole.ADMIN), shipmentController.getShipments);
+router.get('/:id', validateToken, hasRole(UserRole.ADMIN), shipmentController.getShipmentById);
+router.get('/order/:orderId', validateToken, hasRole(UserRole.ADMIN), shipmentController.getShipmentsByOrderId);
+router.post('/', validateToken, hasRole(UserRole.ADMIN), shipmentController.createShipment);
+router.put('/:id', validateToken, hasRole(UserRole.ADMIN), shipmentController.updateShipment);
+router.delete('/:id', validateToken, hasRole(UserRole.ADMIN), shipmentController.deleteShipment);
 
 export default router;
